b01a655cd829e719efa72c42cc5af0263af71110
Tom <thomas.bozon@campu_numerique.fr>
2019-04-02 09:48:37 +0200

    Exo-1 - Les variables ok

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,8 @@
 // you can write js here
 console.log('hello from file');
+var kelvin = prompt("Quelle est la température en Kelvin aujourd'hui ?");
+var celsius = kelvin - 273;// la temperture celsius est toujours inferieur de 273 à kelvin
+var fahrenheit = celsius * (9/5) + 32; // on reprend celsius 
+console.log('Celsius '+ celsius)
+console.log('Fahrenheit '+ fahrenheit)
+console.log('Fahrenheit arrondi '+ Math.floor( fahrenheit))
