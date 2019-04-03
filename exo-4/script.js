62edb474574fd33ad094af583238b52c3a81acf1
Tom <thomas.bozon@campu_numerique.fr>
2019-04-03 09:27:25 +0200

    Exo-4 - Les tableaux. ok

--- exo-4/script.js
+++ exo-4/script.js
@@ -5,11 +5,11 @@ console.log('exo-4');
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time", "it's", "about", "what", "you", "can", "figure", "out.", "-2015", "Chris", "Pine", "Learn", "JavaScript"];
  secretMessage.pop(); // remove Orange (from the end)
  secretMessage.push('to', 'program'); // rajoute un mot
- secretMessage.splice(6, 1, 'right' ); 
- secretMessage.shift();
- secretMessage.unshift('Programming')
+ secretMessage.splice(6, 1, 'right' ); // this is how to remove an item
+ secretMessage.shift(); // remove Apple from the front
+ secretMessage.unshift('Programming')// add to the front
  secretMessage.splice(5,5, 'know');
 
-console.log(secretMessage.join(' '));
+console.log(secretMessage.join(' '));// lie les mots du tableau
 
 console.log(secretMessage);
